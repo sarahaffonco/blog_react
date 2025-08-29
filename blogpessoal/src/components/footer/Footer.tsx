0d5f5b34a3ca2e345c22ab2c423fb4204d28b69e
Sarah Affonco <sarah.affonco@gmail.com.br>
2025-08-29 18:26:55 -0300

    criado login e cadastro

--- blogpessoal/src/components/footer/Footer.tsx
+++ blogpessoal/src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 import {
-  FacebookLogo,
-  InstagramLogo,
-  LinkedinLogo,
+  FacebookLogoIcon,
+  InstagramLogoIcon,
+  LinkedinLogoIcon,
 } from "@phosphor-icons/react";
 
 function Footer() {
@@ -15,11 +15,17 @@ function Footer() {
             Blog Pessoal Generation | Copyright: {data}
           </p>
           <p className="text-lg">Acesse nossas redes sociais</p>
-          <div className="flex gap-2">
-            <LinkedinLogo size={48} weight="bold" />
-            <InstagramLogo size={48} weight="bold" />
-            <FacebookLogo size={48} weight="bold" />
-          </div>
+<div className='flex gap-2'>
+	<a href="https://www.linkedin.com/in/sarah-jorge-affonco/" target="_blank">
+    	<LinkedinLogoIcon size={48} weight='bold' />
+    </a>
+    <a href="https://www.instagram.com/sarah_affonco/" target="_blank">
+    	<InstagramLogoIcon size={48} weight='bold' />
+    </a>
+    <a href="https://web.facebook.com/sarah.affonco/" target="_blank">
+    	<FacebookLogoIcon size={48} weight='bold' />
+	</a>
+</div>
         </div>
       </div>
     </>
