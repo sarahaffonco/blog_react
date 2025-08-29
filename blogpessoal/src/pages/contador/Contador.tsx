7811e93618a96e72a76640593367d9271fa67ec0
Sarah Affonco <sarah.affonco@gmail.com.br>
2025-08-29 18:01:31 -0300

    criado footer e navbar

---
+++ blogpessoal/src/pages/contador/Contador.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function Contador() {
+  const [valor, setValor] = useState(0);
+
+  function handleClick() {
+    setValor(valor + 1);
+  }
+
+  return (
+    <>
+      <div>
+        <h2>Componente Contador</h2>
+        <p>O valor atual é: {valor}</p>
+        <button onClick={handleClick}>Adicionar 1</button>
+      </div>
+    </>
+  );
+}
+
+export default Contador;
---
+++ blogpessoal/src/pages/login/Login.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import Home from "../home/Home";
+
+function Login() {
+  const [isLogged, setIsLogged] = useState(false);
+
+  return (
+    <>
+      {isLogged ? (
+        <Home />
+      ) : (
+        <div>
+          <h2>Componente Login</h2>
+          <button onClick={() => setIsLogged(true)}>Entrar</button>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Login;
